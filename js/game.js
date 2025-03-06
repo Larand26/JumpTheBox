3ab0eac67aaa43da5a38f3d28463a415adf8b289
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-03-06 08:14:42 -0300

    coloca colisao lateral

--- js/game.js
+++ js/game.js
@@ -13,12 +13,24 @@ import { left, right, space } from "./input.js";
 const player = new Player("larand");
 const box1 = new Box(150, 0, 50, 50);
 
+const colision = () => {
+  if (player.x + player.width == box1.x && right) {
+    return true;
+  } else if (player.x == box1.x + box1.width && left) {
+    return true;
+  }
+  return false;
+};
+
 const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpa o canvas antes de redesenhar
-  player.walk(right, left);
+  if (right && !colision()) {
+    player.walk(right, left);
+  } else if (left && !colision()) {
+    player.walk(right, left);
+  }
   drawPlayer(ctx, player);
   drawBox(ctx, box1);
-
   requestAnimationFrame(gameLoop);
 };
 
