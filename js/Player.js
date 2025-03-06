b114b8c773f6bdba8deac011262b71d79d0aab04
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-03-06 09:49:45 -0300

    feat: player cai

--- js/Player.js
+++ js/Player.js
@@ -3,15 +3,25 @@ class Player {
     this.name = name;
     this.width = 50;
     this.height = 50;
-    this.x = 50;
-    this.y = 0;
+    this.x = 150;
+    this.y = 200;
     this.speed = 5;
     this.strongJump = 100;
     this.inAir = false;
+    this.gravity = -0.5;
   }
   walk(r, l) {
     if (r) this.x += this.speed;
     if (l) this.x -= this.speed;
+  }
+  fall() {
+    if (this.inAir) {
+      this.gravity -= 0.2;
+      this.y += this.gravity;
+    } else {
+      this.gravity = -0.5;
+    }
+    if (this.y < 0) this.y = 0;
   }
 }
 export default Player;
--- js/game.js
+++ js/game.js
@@ -11,7 +11,11 @@ import { drawPlayer, drawBox } from "./draw.js";
 import { left, right, space } from "./input.js";
 
 const player = new Player("larand");
-const boxes = [new Box(150, 0, 50, 50)];
+const boxes = [
+  new Box(150, 0, 50, 50),
+  new Box(200, 0, 50, 50),
+  new Box(250, 0, 50, 50),
+];
 
 const colision = () => {
   if (
@@ -30,6 +34,28 @@ const colision = () => {
   return false;
 };
 
+const checkInAir = () => {
+  let air = true;
+  if (player.y == 0) {
+    air = false;
+  }
+  if (
+    boxes.some((box) => player.y <= box.y + box.height + 1) &&
+    boxes.some((box) => player.y - box.y + box.height > box.y + box.height + 40)
+  ) {
+    if (
+      (boxes.some((box) => player.x + player.width <= box.x + box.width) &&
+        boxes.some((box) => player.x + player.width >= box.x)) ||
+      (boxes.some((box) => player.x >= box.x) &&
+        boxes.some((box) => player.x <= box.x + box.width))
+    ) {
+      air = false;
+      player.y = boxes[0].y + boxes[0].height + 0.01;
+    }
+  }
+  return air;
+};
+
 const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpa o canvas antes de redesenhar
   if (right && !colision()) {
@@ -37,6 +63,8 @@ const gameLoop = () => {
   } else if (left && !colision()) {
     player.walk(right, left);
   }
+  player.inAir = checkInAir();
+  player.fall();
   drawPlayer(ctx, player);
   drawBox(ctx, boxes);
   requestAnimationFrame(gameLoop);
