7aad6b7e405ac3ec7fa0be80faaa14f827c73bca
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-03-06 08:00:42 -0300

    feat: cria a caixa sem colisao

--- js/draw.js
+++ js/draw.js
@@ -2,4 +2,8 @@ const drawPlayer = (ctx, player) => {
   ctx.fillStyle = "#f1f1f1";
   ctx.fillRect(player.x, player.y, player.width, player.height);
 };
-export { drawPlayer };
+const drawBox = (ctx, box) => {
+  ctx.fillStyle = "#05abba";
+  ctx.fillRect(box.x, box.y, box.width, box.height);
+};
+export { drawPlayer, drawBox };
--- js/game.js
+++ js/game.js
@@ -5,16 +5,20 @@ ctx.scale(1, -1);
 
 // Imports
 import Player from "./Player.js";
-import { drawPlayer } from "./draw.js";
+import Box from "./Box.js";
+import { drawPlayer, drawBox } from "./draw.js";
 // Teclas
 import { left, right, space } from "./input.js";
 
 const player = new Player("larand");
+const box1 = new Box(150, 0, 50, 50);
 
 const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpa o canvas antes de redesenhar
   player.walk(right, left);
-  drawPlayer(ctx, player, canvas);
+  drawPlayer(ctx, player);
+  drawBox(ctx, box1);
+
   requestAnimationFrame(gameLoop);
 };
 
